96047d2d7d61082e823d82d73775fe7465ba44c8
jangidjay <jangid321jay@gmail.com>
2021-12-27 10:23:25 +0530

    React Event Handler

--- src/index.js
+++ src/index.js
@@ -7,34 +7,56 @@ import "./index.css";
 // Global Vars
 const books = [
   {
+    id: 1,
     img: "https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg",
     title: "The Alchemist",
     author: "Paulo Coelho",
   },
   {
+    id: 2,
     img: "https://images-eu.ssl-images-amazon.com/images/I/41%2BgrDTP2FL._AC_UL200_SR200,200_.jpg",
     title: "DO EPIC SHIT",
     author: "Ankur Warikoo",
   },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg",
+    title: "Ikigai",
+    author: "Héctor García",
+  },
 ];
 
-const names = ["john", "peter", "susan"];
-const newName = names.map((name) => {
-  return <h1>{name}</h1>
-});
-console.log(newName);
-
 function BookList() {
-  return <section className="booklist">{newName}</section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("Clicked!");
+  };
+  const comExample = (author) => {
+    alert(author);
+  };
+
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={clickHandler}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        example
+      </button>
+      <button type="button" onClick={() => comExample(author)}>
+        Complex Example
+      </button>
     </article>
   );
 };
